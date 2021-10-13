3c19992c0ef203a99d0e2326ac809060be9ff4bd
jisu_jeong <jisujeong@jisu-jeongui-MacBookPro.local>
2021-10-13 22:33:40 +0900

    ver1.0

---
+++ src/routes/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>
+        잘 하고 있어.
+      </span>
+    </div>
+  );
+}
+
+export default About;
