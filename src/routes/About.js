94453870682b9ae2d72d4604adf01e98818e04b1
jisu_jeong <jisujeong@jisu-jeongui-MacBookPro.local>
2021-10-26 22:33:39 +0900

    fix error in sharing

--- src/routes/About.js
+++ src/routes/About.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <div className="about__container">
           <span>
-              잘 하고 있어.
+              광고 클릭은 개발자에게 큰 돈이 됩니다.
           </span>
     </div>
   );
